9208862b61ca89f127ada2b745dce48cffc153a7
Shadaf Hossain <shadafhossain01@gmail.com>
2026-05-01 01:08:07 +0600

    Protected Route Design

---
+++ client/src/components/common/ProtectedRoute.jsx
@@ -0,0 +1,14 @@
+import { useSelector } from "react-redux";
+import { Navigate } from "react-router";
+
+const ProtectedRoute = ({ children }) => {
+  const user = useSelector((state) => state.user);
+
+  if (!user) {
+    return <Navigate to="/login" />;
+  }
+
+  return children;
+};
+
+export default ProtectedRoute;
